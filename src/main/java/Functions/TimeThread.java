a0117f30d43818180fc4e99c59c87cf67c835c85
DJZK <emperorxizzle99@gmail.com>
2022-03-21 20:23:01 +0800

    Added time thread dialogues

--- src/main/java/Functions/TimeThread.java
+++ src/main/java/Functions/TimeThread.java
@@ -16,35 +16,33 @@ public class TimeThread {
     }
 
     /**
-     *
      * @return Formatted Time (Hour:Minute AM/PM)
      */
-    public static String getTime(){
+    public static String getTime() {
         DateFormat toTime = new SimpleDateFormat("hh:mm a");
         Date date = new Date();
         return toTime.format(date).toUpperCase();
     }
 
-    public static String getNumericalTime(char func){
+    public static String getNumericalTime(char func) {
         Date time = new Date();
         DateFormat toTime = new SimpleDateFormat("HHmm");
         DateFormat firstTwo = new SimpleDateFormat("HH");
 
-        switch(func){
+        switch (func) {
             case 'a':
                 return toTime.format(time);
             case 'b':
                 return firstTwo.format(time);
             default:
-                return  time.getTime() + "";
+                return time.getTime() + "";
         }
     }
 
     /**
-     *
      * @return Formatted Date (Month Day Year)
      */
-    public static String getDate(){
+    public static String getDate() {
         DateFormat toTime = new SimpleDateFormat("M/dd/yyyy");
         Date date = new Date();
         return toTime.format(date);
@@ -52,34 +50,54 @@ public class TimeThread {
 
 
     // For the realtime clock
-    public void realtime(){
+    public void realtime() {
         Timer t = new Timer();
 
         t.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 int NumTime = Integer.parseInt(getNumericalTime('a'));
-                String GuildID = DatabaseParameters.getGuildID();
-                String TextChannelID = DatabaseParameters.getChannelID();
+
 
                 // Timed Task
-                switch (NumTime){
-                    case 1000:
-                        api.getGuildById(GuildID).getTextChannelById(TextChannelID).sendMessage("It's 10 AM @everyone. You might wanna take a break?").queue();
+                switch (NumTime) {
+                    case 600:
+                        sendChannel("Good morning @everyone. Coffee?");
                         break;
-                    case 1500:
-                        api.getGuildById(GuildID).getTextChannelById(TextChannelID).sendMessage("It's 3 PM @everyone. Let's sip a coffee? only 15 minutes though.").queue();
+                    case 800:
+                        sendChannel("Time to get in! @everyone. I'll only give 30 minutes grace period!");
+                        break;
+                    case 1000:
+                        sendChannel("It's 10 AM @everyone. You might wanna take a break?");
                         break;
                     case 1200:
-                        api.getGuildById(GuildID).getTextChannelById(TextChannelID).sendMessage("It's 12 PM! Time to get out for lunch! @everyone").queue();
+                        sendChannel("It's 12 PM! Time to get out for lunch! @everyone");
                         break;
                     case 1300:
-                        api.getGuildById(GuildID).getTextChannelById(TextChannelID).sendMessage("@everyone 1 PM mark. Time to get back!").queue();
-
+                        sendChannel("@everyone 1 PM mark. Time to get back!");
+                        break;
+                    case 1500:
+                        sendChannel("It's 3 PM @everyone. Let's sip a coffee? only 15 minutes though.");
+                        break;
+                    case 1700:
+                        sendChannel("It's log off time! Don't work too hard come on... Love yourself...");
+                        break;
+                    case 1900:
+                        sendChannel("Take care @everyone!");
+                        break;
+                    case 2200:
+                        sendChannel("Goodnight! @here");
                 }
 
             }
-        },60000,60000);
+        }, 60000, 60000);
+    }
+
+    private void sendChannel(String message) {
+        String GuildID = DatabaseParameters.getGuildID();
+        String TextChannelID = DatabaseParameters.getChannelID();
+        api.getGuildById(GuildID).getTextChannelById(TextChannelID).sendMessage(message).queue();
+
     }
 
 }
